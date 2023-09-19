e01b6867de314e0154a10f9eaf9def8816e432b0
shikoliamos31 <amosshikoli@gmail.com>
2023-09-19 12:40:19 -0700

    task 3

--- handle_format.c
+++ handle_format.c
@@ -12,27 +12,28 @@ int handle_format_specifier(va_list args, const char **format)
 	char buffer[12]; /* Buffer to store the integer as a string */
 	int len = 0;
 	int number;
-	int c;
-	unsigned int unint_num; /* Variable to store unsigned integer arguments */
-
+	unsigned int unint_num; /* Initialize a variable to store unsigned integer arguments */
 	(*format)++; /* Move past '%' */
-
 	switch (**format)
 	{
 		case 'c':
-			char_print += write(1, &c, 1); /* Write the char */
+			/* Handle character specifier */
+			char_print += write(1, &(va_arg(args, int)), 1); /* Write the character */
 			break;
 		case 's':
+			/* Handle string specifier */
 			char_print += write(1, va_arg(args, char *), 0); /* Write the string */
 			break;
 		case '%':
+			/* Handle '%' specifier */
 			char_print += write(1, "%", 1); /* Write '%' */
 			break;
 		case 'd':
 		case 'i':
+			/* Handle integer specifiers ('d' and 'i') */
 			number = va_arg(args, int); /* Get the integer argument */
-			char_print += number < 0 ? write(1, "-", 1) : 0;
-			number = number < 0 ? -number : number;
+			char_print += number < 0 ? write(1, "-", 1) : 0; /* Write '-' for negative numbers */
+			number = number < 0 ? -number : number; /* Make num positive for conversion */
 			do {
 				buffer[len++] = number % 10 + '0'; /* Extract and convert digits */
 				number /= 10;
@@ -43,7 +44,7 @@ int handle_format_specifier(va_list args, const char **format)
 			break;
 		case 'b':
 			/* Handle custom binary specifier '%b' */
-			unint_num = va_arg(args, unsigned int);
+			unint_num = va_arg(args, unsigned int); /* Get the unsigned integer argument */
 			if (unint_num == 0)
 			{
 				char_print += write(1, "0", 1); /* Handle the special case of 0 */
@@ -51,14 +52,50 @@ int handle_format_specifier(va_list args, const char **format)
 			}
 			while (unint_num > 0)
 			{
-				buffer[len++] = (unint_num & 1) + '0';
-				unint_num >>= 1;
-			}
-			while (len > 0)
-				char_print += write(1, &buffer[--len], 1); /* In reverse order */
-			break;
-		default: /* Handle unknown specifier */
-			char_print += write(1, "%", 1) + write(1, *format, 1);
+				buffer[len++] = (unint_num & 1) + '0'; /* Extract and convert binary digits */
+				case 'u':
+				unint_num = va_arg(args, unsigned int);
+            do {
+		    buffer[len++] = unint_num % 10 + '0';
+                unint_num /= 10;
+	    } while (unint_num > 0);
+            while (len > 0)
+                char_print += write(1, &buffer[--len], 1);
+            break;
+        case 'o':
+            unint_num = va_arg(args, unsigned int);
+            do {
+                buffer[len++] = (unint_num % 8) + '0';
+                unint_num /= 8;
+            } while (unint_num > 0);
+	    while (len > 0)
+                char_print += write(1, &buffer[--len], 1);
+            break;
+        case 'x':
+            unint_num = va_arg(args, unsigned int);
+	    do {
+		    int digit = unint_num % 16;
+		    
+		    buffer[len++] = digit < 10 ? digit + '0' : digit - 10 + 'a';
+		    unint_num /= 16;
+            } while (uint_num > 0);
+	    while (len > 0)
+		    count += write(1, &buffer[--len], 1);
+	    break;
+	case 'X':
+	    unint_num = va_arg(args, unsigned int);
+            do {
+                int digit = unint_num % 16;
+		buffer[len++] = digit < 10 ? digit + '0' : digit - 10 + 'A';
+		
+		unint_num /= 16;
+	    } while (unint_num > 0);
+            while (len > 0)
+                char_print += write(1, &buffer[--len], 1);
+            break;
+		default:
+			/* Handle unknown specifier */
+			char_print += write(1, "%", 1) + write(1, *format, 1); /* Print the '%' character if unknown specifier */
 	}
 	return (char_print);
 }
